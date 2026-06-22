1ff13dcd0e860237c0798e28b2292854755da128
saikrishna <saipandu6969@gmail.com>
2026-06-22 23:40:17 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://127.0.0.1:8000";
+const BACKEND_URL =
+  process.env.REACT_APP_BACKEND_URL || "https://api.thegrindstation.in";
 
 export const API_BASE = `${BACKEND_URL}/api`;
 
